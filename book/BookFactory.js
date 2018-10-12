2e309e7a5ff72410b634aadfb0787f0811235d55
Nguyen Manh <manhnv@sphinx-software.com>
2018-10-12 12:02:43 +0700

    create conflict

--- book/BookFactory.js
+++ book/BookFactory.js
@@ -1,7 +1,7 @@
 class BookFactory {
 
-    makeFromRequest(data1) {
-        let book = new Book(data1.title);
-        book.setAuthor(data1.author);
+    makeFromRequest(data2) {
+        let book = new Book(data2.title);
+        book.setAuthor(data2.author);
     }
 }
